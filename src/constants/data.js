e19f66923bc7e67638be311fa1e9daab61dc1d42
Juan Diego Villar Hernandez <juandiegom78@gmail.com>
2024-07-24 21:28:41 -0400

    Pull Eday

---
+++ src/constants/data.js
@@ -0,0 +1,131 @@
+import PinMapIcon from "../icons/PinMapIcon.astro";
+
+export const features = [
+  {
+    icon: PinMapIcon,
+    title: "Zero Configuration",
+    description:
+      "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Sed est non feugiat sagittis.",
+  },
+  {
+    icon: PinMapIcon,
+    title: "Code Security",
+    description:
+      "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Sed est non feugiat sagittis.",
+  },
+  {
+    icon: PinMapIcon,
+    title: "Team Management",
+    description:
+      "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Sed est non feugiat sagittis.",
+  },
+  {
+    icon: PinMapIcon,
+    title: "Access Controlled",
+    description:
+      "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Sed est non feugiat sagittis.",
+  },
+  {
+    icon: PinMapIcon,
+    title: "Access Controlled",
+    description:
+      "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Sed est non feugiat sagittis.",
+  },
+  {
+    icon: PinMapIcon,
+    title: "Access Controlled",
+    description:
+      "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Sed est non feugiat sagittis.",
+  },
+];
+
+export const advantages = [
+  {
+    image: "/images/highlights-1.png",
+    tag: "Nibiru",
+    title: "Responsive Template",
+    description:
+      "This creative and modern mobile responsive website template helps you create any kind of marketing websites or landing pages.",
+  },
+  {
+    image: "/images/highlights-1.png",
+    tag: "Creative",
+    title: "Next Level Template",
+    description:
+      "This new kind of website template integrates your favorite tools and workflows.",
+  },
+  {
+    image: "/images/highlights-2.png",
+    tag: "Template",
+    title: "Modern Design",
+    description:
+      "Feeling old? Try this template and be a part of the modern design.",
+  },
+  {
+    image: "/images/highlights-1.png",
+    tag: "Product",
+    title: "Highlight Features",
+    description:
+      "You can highlight your features in a trendy grid like this one.",
+  },
+  {
+    image: "/images/highlights-3.png",
+    tag: "Webhooks",
+    title: "100+ Integrations",
+    description:
+      "This template can be integrated with hundreds of tools using Zapier.",
+  },
+];
+
+export const servicesData = [
+  {
+    image: "/images/service.webp",
+    subtitle: "Services We Offer",
+    title:
+      "Experience the Ultimate Boost in Productivity with Our Intuitive Platform",
+    servicesList: [
+      "Cloud Databases",
+      "Website Hosting",
+      "Remote Desktop",
+      "File Backup",
+    ],
+  },
+  {
+    image: "/images/service.webp",
+    subtitle: "Our Services",
+    title: "Best IT & technology service in your area",
+    servicesList: [
+      "Design & Development",
+      "Android Apps Development",
+      "Laravel Web Development",
+      "React Web Development",
+    ],
+  },
+];
+
+export const postsList = [
+  {
+    image: "/images/blog-1.jpg",
+    title: "How is technology working with new things?",
+    tags: {
+      category: "Technology",
+      date: "25 Feb, 2022",
+    },
+  },
+  {
+    image: "/images/blog-2.jpg",
+    title: "Top 10 important tips on IT services & design",
+    tags: {
+      category: "Design",
+      date: "25 Feb, 2022",
+    },
+  },
+  {
+    image: "/images/blog-3.jpg",
+    title: "How our company works in different ways",
+    tags: {
+      category: "Startup",
+      date: "25 Feb, 2022",
+    },
+  },
+];
